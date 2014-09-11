4b4acbd47e862c6c311385529ccec30248e2bfb1
Phil Peng <phariel@gmail.com>
2014-09-12 01:43:20 +0800

    refine

--- lock-basic.js
+++ lock-basic.js
@@ -1,26 +1,33 @@
-function stringToUnicode(buffer) {
-	var result = "";
-	buffer.forEach(function (v) {
-		result += "\\u" + ("000" + v.charCodeAt(0).toString(16)).substr(-4);
-	});
-	return result;
+var BASIC_TAG = "0001";
+
+function lock(buffer) {
+    var result = "";
+    for (var i = 0; i < buffer.length; i++) {
+        result += ("000" + buffer[i].charCodeAt(0).toString(16)).substr(-4);
+    }
+    return BASIC_TAG + result;
 }
 
-function unicodeToString(buffer) {
-	var result = "";
-	var arr = buffer.split('\\u');
-	arr.forEach(function (v, i) {
-		if (i > 0) {
-			result += String.fromCharCode(parseInt(v, 16));
-		}
-	});
-	return result;
+function unlock(buffer) {
+    var result = "";
+    var temp = "";
+    for (var i = 0; i < buffer.length; i++) {
+        var index = i + 1;
+        if (index > 4) {
+            temp += buffer[i];
+            if (index % 4 === 0) {
+                result += String.fromCharCode(parseInt(temp, 16));
+                temp = "";
+            }
+        }
+    }
+    return result;
 }
 
 exports.lock = function (buffer) {
-	return stringToUnicode(buffer);
+    return lock(buffer);
 };
 
 exports.unlock = function (buffer) {
-	return unicodeToString(buffer);
+    return unlock(buffer);
 };
